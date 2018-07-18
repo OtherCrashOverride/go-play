f0524b66df4b08d4f243b68ad1077c1a3e7b35cb
OtherCrashOverride <OtherCrashOverride@users.noreply.github.com>
2018-07-18 13:32:23 -0500

    Remove smsplus.

--- smsplus-go/flashrom.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-. ${IDF_PATH}/add_path.sh
-esptool.py --chip esp32 --port "/dev/ttyUSB0" --baud $((230400*4)) --after no_reset write_flash -fs detect 0x200000 "$1"
-file=$(basename "$1")
-echo -en "${file}\0" > temp.bin
-esptool.py --chip esp32 --port "/dev/ttyUSB0" --baud $((230400*4)) write_flash -fs detect 0x300000 temp.bin
